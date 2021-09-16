546ceb0949d4894845daf7efa5ad7803292ff7ea
kapil-156 <bsr.kapil@gmail.com>
2021-09-16 15:53:21 +0530

    my commit

--- backend/server.js
+++ backend/server.js
@@ -13,7 +13,7 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-mongoose.connect( 'mongodb+srv://localhost/kapil1235', {
+mongoose.connect(process.env.MONGODB_URL || 'mongodb://localhost/kssd', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
